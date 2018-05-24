5717045c521c6856e4ca7063fa197baf74d66b27
Hyoungjae Won <surfwon@gmail.com>
2018-05-24 22:07:02 +0900

    1장 자바의 시작

---
+++ Chapter01/src/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * 작성자: 홍길동
+ * 작성일: 2015.12.31
+ * 설  명: 처음 작성한 자바 출력문 
+ */
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, welcom to the java world!");
+	}
+}
---
+++ Chapter01/src/RunStatementExample.java
@@ -0,0 +1,10 @@
+// 실행문과 세미콜론(;)
+public class RunStatementExample {
+	public static void main(String[] args) {
+		int x = 3;  // 변수 x를 선언하고 3을 저장
+		int y = 4;  // 변수 y를 선언하고 4를 저장
+		int result = x + y;  // 변수 result를 선언하고 x와 y를 더한 값을 저장
+		
+		System.out.println(x + "+" + y + "=" + result);  // 콘솔에 출력하는 메소드 추가
+	}
+}
